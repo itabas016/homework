8b665156f3e2f0de00d590a92484fa17827833c1
Roger.Cui <jesuie@live.cn>
2016-12-09 11:01:38 +0800

    update question4 logstic

--- Exercise/Exercise/unit_2/question_4.h
+++ Exercise/Exercise/unit_2/question_4.h
@@ -6,14 +6,54 @@
 
 # include <stdio.h>
 
-
-
-void question_4() {
-	int i;
-	double s, max, min, avg;
-	double arr[9];
-	for (i = 1; i < 10; i++) {
-		printf("Input your score: ");
-		scanf("%f", &s);
+float bubble_sort(float arr[]) {
+	int i, j, temp;
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < i; j++)
+		{
+			if (arr[j] > arr[i])
+			{
+				temp = arr[j];
+				arr[j] = arr[i];
+				arr[i] = temp;
+			}
+		}
 	}
+}
+
+float calculate_avg(float arr[]) {
+	int i;
+	float s = 0;
+	for (i = 1; i < 8; i++)
+	{
+		s += arr[i];
+	}
+	return s / 7;
+}
+
+
+void main() {
+	int i;
+	float s, max, min, avg;
+	float arr[9];
+	for (i = 1; i < 10; i++) {
+		printf("Read the %d referee score: ", i);
+		scanf_s("%f", &s);
+	}
+
+	bubble_sort(arr);
+	min = arr[0];
+	max = arr[8];
+	avg = calculate_avg(arr);
+
+	printf("scores list(min->max):\n");
+	for (i = 0; i < 9; i++)
+	{
+		printf("%.2f ", arr[i]);
+	}
+	printf("\n");
+	printf("Remove the max score: %.2f\n", max);
+	printf("Remove the min score: %.2f\n", min);
+	printf("The avg score: %.2f\n", avg);
 }
